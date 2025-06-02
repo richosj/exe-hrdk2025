e3395000c2c212560eb5da8a6345696e12765036
richosj <okm760596@gmail.com>
2025-06-02 16:59:04 +0900

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,1 @@
+import{t,b as n}from"./ui.js";document.addEventListener("DOMContentLoaded",()=>{console.log("🟢 index.js 로드됨!"),t(".main-tab"),n(".main-button")});
---
+++ assets/js/sub.js
@@ -0,0 +1,1 @@
+import{t as o}from"./ui.js";document.addEventListener("DOMContentLoaded",()=>{console.log("🟢 sub.js 로드됨!"),o(".sub-tab")});
